eb4848ed53df4d374a7699076666b045d7c71178
joe <joe.ulica@gmail.com>
2023-07-12 06:43:37 -0400

    add delete card action

--- src/components/editCard.js
+++ src/components/editCard.js
@@ -1,3 +1,4 @@
+import { upload } from '@testing-library/user-event/dist/upload'
 import axios from 'axios'
 
 export default function CardEdit(props) {
@@ -16,6 +17,18 @@ export default function CardEdit(props) {
       props.setCards(updatedCards)
     })
   }
+
+  function deleteCard() {
+    console.log(props.card.id)
+    axios.delete(`http://localhost:3000/cards/${props.card.id}`).then(response => {
+      console.log(response)
+      props.handleClose()
+      var updatedCards = props.cards
+      updatedCards.splice(props.cardIndex, 1)
+      props.setCards(updatedCards)
+    })
+  }
+
   return (
     <div className="card-new">
       <form onSubmit={handleSubmit}>
@@ -26,7 +39,7 @@ export default function CardEdit(props) {
           Description: <input name="description" defaultValue={props.card.description} />
         </div>
         <button type="submit" style={{ color: "white", backgroundColor: "green" }}>Edit card</button>
-        <button onClick={console.log(props.cards, props.cardIndex, props.card)}>print card</button>
+        <button type="button" onClick={deleteCard} style={{ color: "white", backgroundColor: "red" }}>Delete card</button>
       </form>
     </div >
   )
